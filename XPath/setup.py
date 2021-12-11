ea2f350f88967d549b9bc4aea40b49a74eba9e5e
Arnold Bloemert <arnold.bloemert@nl.bosch.com>
2021-12-11 14:01:39 +0100

    Set version to 0.2.4 and update changelog

--- XPath/setup.py
+++ XPath/setup.py
@@ -60,7 +60,7 @@ class build_py(_build_py):
         return modules
 
 setup(name="py-dom-xpath-six",
-      version="0.2.3",
+      version="0.2.4",
       description="XPath for DOM trees",
       long_description="""\
 py-dom-xpath is a pure Python implementation of XPath 1.0. It
