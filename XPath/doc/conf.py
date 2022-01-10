61a5c12beb83032f5529d66b96672e8adfc9834c
Jack Jansen <Jack.Jansen@cwi.nl>
2022-01-10 01:07:07 +0100

    Converted to python3

--- XPath/doc/conf.py
+++ XPath/doc/conf.py
@@ -41,8 +41,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'XPath'
-copyright = u'2009, Nominum, Inc.'
+project = 'XPath'
+copyright = '2009, Nominum, Inc.'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -168,8 +168,8 @@ htmlhelp_basename = 'XPathdoc'
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, document class [howto/manual]).
 latex_documents = [
-  ('index', 'XPath.tex', ur'XPath Documentation',
-   ur'Damien Neil', 'manual'),
+  ('index', 'XPath.tex', r'XPath Documentation',
+   r'Damien Neil', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
